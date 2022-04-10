f491fa7453fe24b23b0b84c3c3a8c43505bf6fd3
fuhumetha <85863244+fuhumetha@users.noreply.github.com>
2022-04-10 17:15:18 +0000

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -1,4 +1,5 @@
 TRACKERS=$(curl -Ns https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all_udp.txt | awk '$1' | tr '\n' ',')
+
 aria2c \
 --allow-overwrite=true \
 --bt-enable-lpd=true \
@@ -8,9 +9,11 @@ aria2c \
 --daemon=true \
 --enable-rpc \
 --follow-torrent=mem \
---max-connection-per-server=8 \
+--max-connection-per-server=16 \
 --max-overall-upload-limit=1K \
 --peer-agent=qBittorrent/4.3.6 \
 --peer-id-prefix=-qB4360- \
 --seed-time=0 \
+--bt-tracker-connect-timeout=300 \
+--bt-stop-timeout=1200 \
 --user-agent=qBittorrent/4.3.6
